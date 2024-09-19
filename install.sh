79af9fe13015118bc0261c6484363570e26f822f
aurennunes <auren1207@gmail.com>
2024-09-19 22:11:55 +0100

    feat: install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "$OS" == "Linux" ]]; then
+    BINARY="commit-wizard-linux-amd64"
+elif [[ "$OS" == "Darwin" ]]; then
+    BINARY="commit-wizard-macos-amd64"
+elif [[ "$OS" == "MINGW"* || "$OS" == "CYGWIN"* ]]; then
+    BINARY="commit-wizard-windows-amd64.exe"
+else
+    echo "Sistema não suportado!"
+    exit 1
+fi
+
+REPO_URL="https://github.com/usuario/commit-wizard/releases/download/v1.0.0/$BINARY"
+
+echo "Baixando CommitWizard para $OS..."
+curl -L -o /usr/local/bin/commit-wizard "$REPO_URL"
+
+chmod +x /usr/local/bin/commit-wizard
+
+echo "CommitWizard instalado com sucesso!"
